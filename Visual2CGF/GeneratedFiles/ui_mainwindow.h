3030a99403b0bf93a0c76e0967940c31fcb1777f
Arnoud van Leeuwen - NLR <arnoud.van.leeuwen@nlr.nl>
2017-08-02 11:37:42 +0200

    Edited .gitignore to exlude some visual studio 2015 files

---
+++ Visual2CGF/GeneratedFiles/ui_mainwindow.h
@@ -0,0 +1,163 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindowClass
+{
+public:
+    QAction *actionSettings;
+    QAction *actionExit;
+    QAction *actionAbout;
+    QWidget *centralWidget;
+    QVBoxLayout *verticalLayout_3;
+    QVBoxLayout *verticalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QComboBox *comboBox;
+    QSpacerItem *verticalSpacer;
+    QListWidget *listWidget_2;
+    QListWidget *listWidget;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QMenu *menuHelp;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindowClass)
+    {
+        if (MainWindowClass->objectName().isEmpty())
+            MainWindowClass->setObjectName(QStringLiteral("MainWindowClass"));
+        MainWindowClass->resize(884, 700);
+        actionSettings = new QAction(MainWindowClass);
+        actionSettings->setObjectName(QStringLiteral("actionSettings"));
+        actionExit = new QAction(MainWindowClass);
+        actionExit->setObjectName(QStringLiteral("actionExit"));
+        actionAbout = new QAction(MainWindowClass);
+        actionAbout->setObjectName(QStringLiteral("actionAbout"));
+        centralWidget = new QWidget(MainWindowClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        verticalLayout_3 = new QVBoxLayout(centralWidget);
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(6);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        comboBox = new QComboBox(centralWidget);
+        comboBox->setObjectName(QStringLiteral("comboBox"));
+        comboBox->setMinimumSize(QSize(100, 0));
+
+        verticalLayout->addWidget(comboBox);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        listWidget_2 = new QListWidget(centralWidget);
+        listWidget_2->setObjectName(QStringLiteral("listWidget_2"));
+
+        horizontalLayout->addWidget(listWidget_2);
+
+
+        verticalLayout_2->addLayout(horizontalLayout);
+
+        listWidget = new QListWidget(centralWidget);
+        listWidget->setObjectName(QStringLiteral("listWidget"));
+        listWidget->setMinimumSize(QSize(0, 200));
+        listWidget->setMaximumSize(QSize(16777215, 500));
+
+        verticalLayout_2->addWidget(listWidget);
+
+
+        verticalLayout_3->addLayout(verticalLayout_2);
+
+        MainWindowClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindowClass);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 884, 21));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        menuHelp = new QMenu(menuBar);
+        menuHelp->setObjectName(QStringLiteral("menuHelp"));
+        MainWindowClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindowClass);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindowClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindowClass);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindowClass->setStatusBar(statusBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuBar->addAction(menuHelp->menuAction());
+        menuFile->addAction(actionSettings);
+        menuFile->addSeparator();
+        menuFile->addAction(actionExit);
+        menuHelp->addAction(actionAbout);
+
+        retranslateUi(MainWindowClass);
+
+        QMetaObject::connectSlotsByName(MainWindowClass);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindowClass)
+    {
+        MainWindowClass->setWindowTitle(QApplication::translate("MainWindowClass", "MainWindow", Q_NULLPTR));
+        actionSettings->setText(QApplication::translate("MainWindowClass", "Settings", Q_NULLPTR));
+        actionExit->setText(QApplication::translate("MainWindowClass", "Exit", Q_NULLPTR));
+        actionAbout->setText(QApplication::translate("MainWindowClass", "About", Q_NULLPTR));
+        comboBox->clear();
+        comboBox->insertItems(0, QStringList()
+         << QApplication::translate("MainWindowClass", "Fixed wing", Q_NULLPTR)
+         << QApplication::translate("MainWindowClass", "Rotary wing", Q_NULLPTR)
+         << QApplication::translate("MainWindowClass", "Submersible", Q_NULLPTR)
+         << QApplication::translate("MainWindowClass", "Surface vessel", Q_NULLPTR)
+         << QApplication::translate("MainWindowClass", "Tracked vehicle", Q_NULLPTR)
+        );
+        menuFile->setTitle(QApplication::translate("MainWindowClass", "File", Q_NULLPTR));
+        menuHelp->setTitle(QApplication::translate("MainWindowClass", "Help", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindowClass: public Ui_MainWindowClass {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
